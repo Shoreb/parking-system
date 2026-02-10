69491eff8441918f0e2d4be7e553916d99c4bdda
Shoreb Pava <shorebpava18@gmail.com>
2026-02-10 17:14:38 -0500

    Configurar conexión MySQL con pool de conexiones

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import { pool } from './config/db.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/health', (req, res) => {
-  res.json({ status: 'OK', message: 'Servidor funcionando correctamente' });
+app.get('/health', async(req, res) => {
+    try {
+    const [result] = await pool.query('SELECT 1');
+    res.json({ status: 'OK', database: 'Conectada' });
+  } catch (error) {
+    res.status(500).json({ status: 'ERROR', database: 'No conecta' });
+  }
 });
 
 export default app;
