9ce7c3f37be76c582ce9d3d59a79b235f9c8a9e5
Shoreb Pava <shorebpava18@gmail.com>
2026-02-19 23:26:17 -0500

    Utilizar mysql_url de railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,5 @@
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const pool = mysql.createPool(process.env.MYSQL_URL);
 
 module.exports = pool;
