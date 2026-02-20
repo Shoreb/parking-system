5c920bfa7ef0d25b5ca34cb61de1f70052001553
Shoreb Pava <shorebpava18@gmail.com>
2026-02-19 21:30:11 -0500

    Configuración inicial del backend con Express, MySQL y sesiones

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(session({
+  secret: 'parqueadero_secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../views')));
+
+app.use('/auth', require('./routes/auth.routes'));
+app.use('/operario', require('./routes/operario.routes'));
+app.use('/admin', require('./routes/admin.routes'));
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado en puerto 3000');
+});
