64f2ba189ba8606ecff6d470cbbedf0f363fe9d2
Shoreb Pava <shorebpava18@gmail.com>
2026-02-19 21:54:04 -0500

    Integración botón flotante de WhatsApp en todas las vistas

---
+++ public/js/whatsapp.js
@@ -0,0 +1,6 @@
+const link = document.createElement("a");
+link.href = "https://wa.me/573000000000";
+link.className = "whatsapp-float";
+link.innerHTML = "W";
+link.target = "_blank";
+document.body.appendChild(link);
